41e8fd01b988c1e95f11577b4b217d9e368a74a5
Mike Kipp <168943773+shadowlord1012@users.noreply.github.com>
2025-10-09 16:17:40 -0400

    Update version 1
    CSS is about 90 percent completed, changed some layout as i found the new one to look nicer

--- ASSIGNMENT01/public/javascripts/js_All_Pages.js
+++ ASSIGNMENT01/public/javascripts/js_All_Pages.js
@@ -1,31 +1,9 @@
 /* an image will follow you around on the screen as your "guild" */
 const body = document.querySelector("html");
 const mouseImage = document.querySelector("#followingMouse");
-const hoverBoxArea = document.getElementById('hoverArea');
-const revealedTextArea = document.getElementById('revealedText');
-const hiddenText = document.getElementById('hiddentText');
 
 //Gets the language of the browser
 var guildClicked = 0;
-if(hoverBoxArea != null)
-    hoverBoxArea.addEventListener("mouseover", function (){
-        if(guildClicked === 1){
-            revealedTextArea.style.display = 'block';
-        }
-        else
-        {
-            hiddenText.setAttribute(this.hidden, false);
-        }
-    });
-if(hoverBoxArea != null)
-    hoverBoxArea.addEventListener("mouseout", function (){
-        if(guildClicked===1){
-            revealedTextArea.style.display = 'none';
-        }
-        else {
-            hiddenText.setAttribute(this.hidden, true);
-        }
-    });
 
 mouseImage.addEventListener("click", addMouseOver)
 
